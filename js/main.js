bc617130d86a128bd49d7a620a18ab1fd23b5ede
Kench <z481592637@gmail.com>
2025-04-30 16:56:29 +0800

    v0.0.0.2，模板設置測試

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// 滾動時觸發動畫
+document.addEventListener('DOMContentLoaded', function() {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.3,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
